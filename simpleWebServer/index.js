5e02075198a0f539a36697164d6591c92381fd55
Jose Murillo <jp1995azul@hotmail.com>
2020-03-11 13:18:29 +0100

    Add nodejs container exercise

---
+++ simpleWebServer/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello world</h1>");
+});
+
+
+app.listen(8080, function() {
+    console.log("Listening on port 8080");
+});
